056f00df4d00eb0cc76e8ccfe7fd3b791fcdb313
MiltonDJC <miltonaxe2001@gmail.com>
2025-11-05 22:08:28 -0300

    Update task model for using id property

--- lib/models/task.dart
+++ lib/models/task.dart
@@ -1,14 +1,21 @@
 class Task {
-  const Task({required this.taskName, this.status = false, this.description});
+  const Task({
+    required this.id,
+    required this.taskName,
+    this.status = false,
+    this.description,
+  });
 
   Task copyWith({String? taskName, bool? status, String? description}) {
     return Task(
+      id: this.id,
       taskName: taskName ?? this.taskName,
       status: status ?? this.status,
       description: description ?? this.description,
     );
   }
 
+  final int id;
   final String taskName;
   final bool status;
   final String? description;
